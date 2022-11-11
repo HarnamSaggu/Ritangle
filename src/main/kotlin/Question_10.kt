a29e00b09d57fa38939c14d2995443e5f0828f08
HarnamSaggu <atoastytoasteronacoaster@gmail.com>
2022-11-11 23:24:56 +0000

    Q10

--- src/main/kotlin/Question_10.kt
+++ src/main/kotlin/Question_10.kt
@@ -1,14 +1,12 @@
-import kotlin.math.acos
 import kotlin.math.pow
-import kotlin.math.sin
+import kotlin.math.sqrt
 
 fun main() {
     val areas = "123456789".permute().map { s ->
         val a: Double = (s[0 + 0].digitToInt() * 100 + s[0 + 1].digitToInt() * 10 + s[0 + 2].digitToInt()).toDouble()
         val b: Double = (s[3 + 0].digitToInt() * 100 + s[3 + 1].digitToInt() * 10 + s[3 + 2].digitToInt()).toDouble()
         val c: Double = (s[6 + 0].digitToInt() * 100 + s[6 + 1].digitToInt() * 10 + s[6 + 2].digitToInt()).toDouble()
-        val theta: Double = acos((a.pow(2) + b.pow(2) - c.pow(2)) / (2 * a * b))
-        val area: Double = 0.5 * a * b * sin(theta)
+        val area: Double = 0.25 * sqrt(4 * a.pow(2) * b.pow(2) - (a.pow(2) + b.pow(2) - c.pow(2)).pow(2))
         Pair(area, listOf(a.toInt(), b.toInt(), c.toInt()))
     }.filter { !it.first.isNaN() && it.first > 0.1 }.sortedBy { it.first }
     println(areas.first())
